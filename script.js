5183889d34525e9d5ccf2b54476da47466d69a84
Vincasmen <vincasmen13@gmail.com>
2023-07-14 19:23:06 +0200

    Finished logick for changing icons on click

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menuList = document.querySelector("nav");
-const hamburgerIcon = document.querySelector(".fa-solid");
+const hamburgerIcon = document.querySelector(".fa-bars");
+
+menuIcon.addEventListener("click", () => {
+  hamburgerIcon.classList.toggle("fa-xmark");
+});
+
+// menuIcon.addEventListener("click", () => {
+//   hamburgerIcon.className = hamburgerIcon.classList.contains("fa-bars")
+//     ? "fa-solid fa-xmark"
+//     : "fa-solid fa-bars";
+// });
