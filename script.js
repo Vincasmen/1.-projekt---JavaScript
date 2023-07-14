073e454f814a84c5a75c396e183933004d9010be
Vincasmen <vincasmen13@gmail.com>
2023-07-14 20:44:31 +0200

    FINISHED

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ const hamburgerIcon = document.querySelector(".fa-bars");
 
 menuIcon.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("fa-xmark");
+  menuList.style.display =
+    menuList.style.display === "block" ? "none" : "block";
 });
+
+checkMediaQuery = () => {
+  if (window.innerWidth > 600) {
+    menuList.style.display = "block";
+  } else {
+    menuList.style.display = "none";
+  }
+};
+
+window.addEventListener("resize", checkMediaQuery);
 
 // menuIcon.addEventListener("click", () => {
 //   hamburgerIcon.className = hamburgerIcon.classList.contains("fa-bars")
